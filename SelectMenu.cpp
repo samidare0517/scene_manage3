f2220a3cc76f22154f5f2de98fd008fd40fb44d5
石橋 叶恵 <fko2247009@stu.o-hara.ac.jp>
2022-10-07 10:00:46 +0900

    プロジェクト ファイルを追加します。

---
+++ SelectMenu.cpp
@@ -0,0 +1,60 @@
+#include "SelectMenu.h"
+
+
+
+//=================================
+//SlectMenu::Item
+//=================================
+
+SelectMenu::Item::Item()
+{
+	m_text = nullptr;
+}
+
+SelectMenu::Item::~Item()
+{
+
+}
+
+void::SelectMenu::Item::setText(const char* text)
+{
+	m_text = text;
+}
+
+
+
+
+//=================================
+//SlectMenu
+//=================================
+
+SelectMenu::SelectMenu()
+{
+
+}
+
+SelectMenu::~SelectMenu()
+{
+
+}
+
+void SelectMenu::init()
+{
+
+}
+
+void SelectMenu::end()
+{
+
+}
+
+
+void SelectMenu::update()
+{
+
+}
+
+void SelectMenu::draw()
+{
+
+}
